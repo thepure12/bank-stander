8d56abce385fd1889f8a6d323415ade146ce3e2c
thepure12 <5061163+thepure12@users.noreply.github.com>
2021-12-08 16:21:50 -0500

    added welcome text to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    Test
+    <div class="mx-auto" style="width: 500px">
+      <h2>Hi There</h2>
+      <p>
+        Welcome, this site generates config for the El Bank Stander plugin. It is in early
+        development, but hopefully you can find something useful. Currently, the only
+        thing implemented is smithing at Varrock west bank. <b>More to come soon!</b>
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
