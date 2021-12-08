5ec11fa5f3aac9d6b35aeabd37f02889ad272211
thepure12 <5061163+thepure12@users.noreply.github.com>
2021-12-08 17:00:20 -0500

    added bug report form and suggestion form

---
+++ pages/bugs.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="d-flex">
+    <div class="mx-auto">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSdDGf-_bbCXg7jr3BwyBK_8GWjIapM3eB9MZrg7BvDnyJWaSw/viewform?embedded=true"
+        width="640"
+        height="740"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+        >Loading…</iframe
+      >
+    </div>
+  </div>
+</template>
---
+++ pages/suggestions.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="d-flex">
+    <div class="mx-auto">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLScXz0aJRaBUoBtMzBlACMPNMhdcfKRkRmwazBd8A6spKjFT_Q/viewform?embedded=true"
+        width="640"
+        height="740"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+        >Loading…</iframe
+      >
+    </div>
+  </div>
+</template>
