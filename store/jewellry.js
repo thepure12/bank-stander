2a981432589bc3de14a24491457c019483398f9b
thepure12 <5061163+thepure12@users.noreply.github.com>
2021-12-09 19:05:04 -0500

    started adding jewelry config

---
+++ store/jewellry.js
@@ -0,0 +1,18 @@
+export const state = () => ({
+    bars: {
+        "Gold": {
+            id: 2357,
+            gems: {"Opal": 1609, "Jade": 1611, "Red Topaz": 1613}
+        },
+        "Silver": {
+            id: 2355,
+            gems: {"Sapphire": 1607, "Emerald": 1605, "Ruby": 1603, "Diamond": 1601, "Dragonstone": 1615, "Onyx": 6573}
+        }
+    },
+    item: {
+        "Ring": {mould: 1592},
+        "Necklace": {mould: 1597},
+        "Bracelet": {mould: 11065},
+        "Amulet": {mould: 1595}
+    }
+});
