96b4573c98946a47e0031eae429ad1937ee36edd
thepure12 <5061163+thepure12@users.noreply.github.com>
2021-12-10 00:05:57 -0500

    added config for gold jewellry up to diamond

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,6 +11,7 @@
           <b-nav-item to="/smithing">Smithing</b-nav-item>
           <b-nav-item to="/herblore">Herblore</b-nav-item>
           <b-nav-item to="/fletching">Fletching</b-nav-item>
+          <b-nav-item to="/jewellry">Jewellry</b-nav-item>
           <b-nav-item to="/bugs">Report Bug</b-nav-item>
           <b-nav-item to="/suggestions">Suggestions</b-nav-item>
         </b-navbar-nav>
--- store/jewellry.js
+++ store/jewellry.js
@@ -2,17 +2,35 @@ export const state = () => ({
     bars: {
         "Gold": {
             id: 2357,
-            gems: {"Opal": 1609, "Jade": 1611, "Red Topaz": 1613}
+            gems: { "Sapphire": 1607, "Emerald": 1605, "Ruby": 1603, "Diamond": 1601, "Dragonstone": 1615, "Onyx": 6573 },
+            startParam: {
+                1592: 29229063,
+                1597: 29229077,
+                1595: 29229090,
+                11065: 29229102
+
+            }
         },
-        "Silver": {
-            id: 2355,
-            gems: {"Sapphire": 1607, "Emerald": 1605, "Ruby": 1603, "Diamond": 1601, "Dragonstone": 1615, "Onyx": 6573}
-        }
+        // "Silver": {
+        //     id: 2355,
+        //     gems: { "Opal": 1609, "Jade": 1611, "Red Topaz": 1613 }
+        // }
     },
-    item: {
-        "Ring": {mould: 1592},
-        "Necklace": {mould: 1597},
-        "Bracelet": {mould: 11065},
-        "Amulet": {mould: 1595}
-    }
+    items: {
+        "Ring": { mould: 1592 },
+        "Necklace": { mould: 1597 },
+        "Amulet": { mould: 1595 },
+        "Bracelet": { mould: 11065 }
+    },
+    banks: [
+        {
+            text: "Edgeville",
+            value: {
+                type: "Npc",
+                id: 1613,
+                opCode: 11,
+                obj: 16469
+            }
+        }
+    ]
 });
