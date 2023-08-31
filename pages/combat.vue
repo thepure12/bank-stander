952c6cacd855ce2a2fc39e1251f25f4fb60590f3
thepure12 <5061163+thepure12@users.noreply.github.com>
2023-08-31 14:51:48 -0400

    added more combat npcs

--- pages/combat.vue
+++ pages/combat.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <h5 class="text-warning">Warning: Does not auto eat!</h5>
+        <!-- <ConfigCard title="Test title">
+            Testing slot
+        </ConfigCard> -->
         <Banks v-model="bank" label="NPC" :banks="combat.banks"></Banks>
         <GeneralConfig :type="3"></GeneralConfig>
         <ItemConfig></ItemConfig>
@@ -13,6 +16,7 @@
 <script>
 import BankConfig from '~/components/BankConfig.vue';
 import Banks from '~/components/Banks.vue';
+import ConfigCard from '~/components/ConfigCard.vue';
 import GeneralConfig from '~/components/GeneralConfig.vue';
 import InvConfig from '~/components/InvConfig.vue';
 import ItemConfig from '~/components/ItemConfig.vue';
@@ -20,7 +24,7 @@ import MenuConfig from '~/components/MenuConfig.vue';
 import ObjectConfig from '~/components/ObjectConfig.vue';
 
 export default {
-    components: { BankConfig, Banks, GeneralConfig, ItemConfig, ObjectConfig, BankConfig, MenuConfig, InvConfig },
+    components: { BankConfig, Banks, GeneralConfig, ItemConfig, ObjectConfig, BankConfig, MenuConfig, InvConfig, ConfigCard },
     data() {
         return {
             bank: null
