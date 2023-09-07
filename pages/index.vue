ff9ddb90077025062691076bd6b261d7a1981b22
thepure12 <5061163+thepure12@users.noreply.github.com>
2023-09-07 15:56:32 -0400

    added wines

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,9 @@
     <div class="mx-auto" style="width: 500px">
       <h2>Hi There</h2>
       <p>
-        Welcome, this site generates config for the El Bank Stander plugin. It is in early
-        development, but hopefully you can find something useful.
+        Welcome, this site generates config for the El Bank Stander plugin. You are currently viewing version 1, which will
+        no longer receive updates.
+        <b-link to="/v2">Please see version 2.</b-link>
       </p>
     </div>
   </div>
--- pages/v2/cooking.vue
+++ pages/v2/cooking.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="mb-3">
+        <!-- <b-form-file v-model="file" placeholder="Select Runelite profile properties file" accept=".properties"
+            @input="fileSelected"></b-form-file> -->
         <ConfigCardV2 v-model="bank" :items="cooking.banks" title="Bank"></ConfigCardV2>
         <ConfigCardV2 v-model="item" :items="cooking.items" title="Item"></ConfigCardV2>
         <ConfigCardV2 v-for="(cfg, name, i) in config" :key="name" :title="getCfgTitle(name)" :config="cfg"></ConfigCardV2>
@@ -14,7 +16,20 @@ export default {
     mixins: [config],
     data() {
         return {
-            cooking: cooking
+            cooking: cooking,
+            file: null
+        }
+    },
+    methods: {
+        fileSelected(file) {
+            file.text()
+                .then(text => {
+                    text.split("\n").forEach(e => {
+                        if (e.includes("ElBank")) {
+                            console.log(e);
+                        }
+                    })
+                })
         }
     },
     created() {
