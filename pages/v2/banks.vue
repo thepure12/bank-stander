d32baf91a5fd57de3840413879b41b80357649a1
TJ Renninger <renningert@LAPTOP-LLEJQRTO>
2023-10-18 14:59:57 -0400

    added banks page

---
+++ pages/v2/banks.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="mb-3">
+        <ConfigCardV2 v-model="bank" :items="banks" title="Bank" visible></ConfigCardV2>
+        <ConfigCardV2 :title="getCfgTitle('bank')" :config="config.bank" visible>
+        </ConfigCardV2>
+    </div>
+</template>
+<script>
+import banks from '~/static/banks.json'
+import config from '~/mixins/config.js'
+export default {
+    name: "Banks",
+    layout: "layoutv2",
+    mixins: [config],
+    data() {
+        return {
+            banks: banks
+        }
+    }
+}
+</script>
