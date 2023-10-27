15ff70396e4770e0121af3310f88cc3a5ebf2cfc
thepure12 <5061163+thepure12@users.noreply.github.com>
2023-10-27 10:26:50 -0400

    removed v1 from nav and upated welcome text

--- pages/v2/index.vue
+++ pages/v2/index.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <div class="mx-auto" style="width: 500px">
-      <h2>Hi There</h2>
+    <div class="mx-auto" style="max-width: 500px">
+      <h3>Hi There!</h3>
       <p>
-        Welcome to version 2. It is still a work in progress, but should allow for things to be more easily added and
-        updated. If you got here on accident you can
-        <b-link to="/">go back to version 1.</b-link>
+        Welcome to Bank Stander setups. Here you can find a myriad of setups for El's
+        <b-link href="discord://discord.com/channels/764897214388240427/1092235746024366182" target="_blank">#🔌🏦bankstander </b-link>. If
+        you can't find what you are looking for or need additional help, you can ask questions in
+        <b-link href="discord://discord.com/channels/764897214388240427/1080789264981303336" target="_blank">#🔌chat</b-link>.
       </p>
     </div>
   </div>
