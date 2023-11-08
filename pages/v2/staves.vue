0d73490634c1b84f8b04734e073aa09f14bf2625
thepure12 <5061163+thepure12@users.noreply.github.com>
2023-11-08 14:57:43 -0500

    added staves

---
+++ pages/v2/staves.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="mb-3">
+        <SaveButtons @click="saveConfig"></SaveButtons>
+        <ConfigCardV2 v-model="bank" :items="banks" title="Bank" visible></ConfigCardV2>
+        <ConfigCardV2 v-model="item" :items="staves.items" title="Item" visible></ConfigCardV2>
+        <ConfigCardV2 v-for="(cfg, name, i) in config" :key="name" :title="getCfgTitle(name)" :config="cfg" visible>
+        </ConfigCardV2>
+    </div>
+</template>
+<script>
+import staves from '~/static/staves.json'
+import banks from '~/static/banks.json'
+import config from '~/mixins/config.js'
+import SaveButtons from '~/components/SaveButtons.vue'
+export default {
+    name: "Staves",
+    layout: "layoutv2",
+    mixins: [config],
+    data() {
+        return {
+            staves: staves,
+            banks: banks
+        };
+    },
+    created() {
+        this.setDefaultConfig(this.staves.config);
+    },
+    components: { SaveButtons }
+}
+</script>
