da3da0b2994f4aeac773a8690a1c636e7b6ef5b2
thepure12 <5061163+thepure12@users.noreply.github.com>
2023-11-11 11:46:02 -0500

    added templates for skiller and shopper

---
+++ pages/skiller/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="mb-3">
+        <p class="h3">Comming soon!</p>
+    </div>
+</template>
+<script>
+export default {
+    name: "Skiller",
+    layout: "layoutv2",
+}
+</script>
