ba18aeae8a6eb403c5df95f7e1d2e92eab49ac8b
TJ Renninger <renningert@LAPTOP-LLEJQRTO>
2023-11-11 11:46:02 -0500

    added templates for skiller and shopper

---
+++ pages/shopper/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="mb-3">
+        <p class="h3">Comming soon!</p>
+    </div>
+</template>
+<script>
+export default {
+    name: "Shopper",
+    layout: "layoutv2",
+}
+</script>
