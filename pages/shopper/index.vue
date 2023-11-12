8afb0aad87f6399b126f4d67f44a2ff99c351bff
TJ Renninger <renningert@LAPTOP-LLEJQRTO>
2023-11-11 20:03:27 -0500

    added lundails rune shop

--- pages/shopper/index.vue
+++ pages/shopper/index.vue
@@ -1,11 +1,87 @@
 <template>
     <div class="mb-3">
-        <p class="h3">Comming soon!</p>
+        <ConfigCardV2 v-model="shop" :items="shopper.shops" title="Shops" visible vselect></ConfigCardV2>
+        <ConfigCardV2 title="Items" visible>
+            <!-- <b-form-checkbox-group v-if="shop" v-model="items" stacked>
+                <b-form-checkbox v-for="item in shop.items" :value="item.value" :key="item.text">
+                    {{ item.text }} {{ item.value.stock }}
+                </b-form-checkbox>
+            </b-form-checkbox-group> -->
+            <b-table v-if="shop" :items="shop.items" :fields="fields" @row-selected="onRowSelected" small responsive>
+                <template #cell(buy_ammount)="data">
+                    <b-form-input v-model="data.item.buy_amount" type="number" size="sm" min="0" :max="data.item.stock"
+                        placeholder="0"></b-form-input>
+                </template>
+            </b-table>
+        </ConfigCardV2>
+        <ConfigCardV2 v-for="(cfg, name, i) in config" :key="name" :title="getCfgTitle(name)" :config="cfg" visible>
+        </ConfigCardV2>
     </div>
 </template>
 <script>
+import shopper from '~/static/shopper.json'
 export default {
     name: "Shopper",
     layout: "layoutv2",
+    data() {
+        return {
+            shopper: shopper,
+            shop: null,
+            fields: ['item', 'stock', 'buy_ammount'],
+        }
+    },
+    computed: {
+        config() {
+            return {
+                general: {
+                    shop: {
+                        label: "Shop",
+                        value: "Custom"
+                    },
+                    packs: {
+                        label: "Open Packs",
+                        value: "☐"
+                    },
+                    stams: {
+                        label: "Staminas",
+                        value: "☐"
+                    },
+                    slots: {
+                        label: "Slots",
+                        value: this.shop ? this.shop.items
+                            .filter(x => x.buy_amount > 0)
+                            .map(x => x.slot)
+                            .join(",") : "0,0,0"
+                    },
+                    quantities: {
+                        label: "Quantities",
+                        value: this.shop ? this.shop.items
+                            .filter(x => x.buy_amount > 0)
+                            .map(x => x.stock - x.buy_amount)
+                            .join(",") : "0,0,0"
+                    }
+                },
+                custom: {
+                    name: {
+                        label: "Shop Name",
+                        value: this.shop ? this.shop.name : "name"
+                    },
+                    location: {
+                        label: "Shop Loc",
+                        value: this.shop ? this.shop.location : "x,y"
+                    },
+                    option: {
+                        label: "Shop Option",
+                        value: this.shop ? this.shop.option : "Third"
+                    }
+                }
+            }
+        }
+    },
+    methods: {
+        getCfgTitle(cfg) {
+            return cfg.charAt(0).toUpperCase() + cfg.slice(1)
+        }
+    }
 }
 </script>
