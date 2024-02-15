a12d959eaa1358246128b3aa731268e587ff4d01
TJ Renninger <renningert@LAPTOP-LLEJQRTO>
2024-02-15 14:56:35 -0500

    added some headers that hopefully prevent caching

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,10 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { 'http-equiv': 'cache-control', content: 'no-cache'},
+      { 'http-equiv': 'expires', content: '0'},
+      { 'http-equiv': 'pragma', content: 'no-cache'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/bank-stander/favicon.ico' }
